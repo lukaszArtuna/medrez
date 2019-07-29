455a8d6b4519699f385b1a999d69580ab2f4eb03
lukaszArtuna <52832681+lukaszArtuna@users.noreply.github.com>
2019-07-29 20:05:38 +0200

    UserService add

--- med/src/pl/medres/service/UserService.java
+++ med/src/pl/medres/service/UserService.java
@@ -1,5 +1,19 @@
 package pl.medres.service;
 
+import pl.medres.dao.DAOFactory;
+import pl.medres.model.User;
+import pl.medres.dao.UserDAO;
+
 public class UserService {
+	public void addUser(Long user_id, String email, String password) {
+		User user = new User();
+		user.setUser_id(user_id);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setIs_active(true);
+		DAOFactory factory = DAOFactory.getDAOFactory();
+		UserDAO userDao = factory.getUserDAO();
+		userDao.create(user);
+	}
 
 }
